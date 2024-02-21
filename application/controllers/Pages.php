7586d443ceaf6dbe8f3bd0accfddff0b6ad05fb8
shakiel17 <hisoka.aboy@gmail.com>
2024-02-21 08:49:09 +0800

    Index

---
+++ application/controllers/Pages.php
@@ -0,0 +1,18 @@
+<?php
+    ini_set('max_execution_time', 0);
+    ini_set('memory_limit','2048M');
+    date_default_timezone_set('Asia/Manila');
+    class Pages extends CI_Controller{
+        public function index(){
+            $page = "index";
+            if(!file_exists(APPPATH.'views/pages/'.$page.".php")){
+                show_404();
+            }
+            if($this->session->user_login){                
+            }else{
+              redirect(base_url());
+            }                                   
+            $this->load->view('pages/'.$page, $data);            
+        }
+    }
+?>
