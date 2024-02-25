014973420404478e012f245f24eb36d26735155b
shakiel17 <114818927+shakiel17@users.noreply.github.com>
2024-02-25 22:05:28 +0800

    Update vlc_db.sql

--- database/vlc_db.sql
+++ database/vlc_db.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Community v13.1.6 (64 bit)
-MySQL - 10.1.38-MariaDB : Database - vlc_db
+SQLyog Community v13.2.1 (64 bit)
+MySQL - 10.1.21-MariaDB : Database - vlc_db
 *********************************************************************
 */
 
@@ -16,16 +16,60 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`vlc_db` /*!40100 DEFAULT CHARACTER SET 
 
 USE `vlc_db`;
 
+/*Table structure for table `advances` */
+
+DROP TABLE IF EXISTS `advances`;
+
+CREATE TABLE `advances` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `empid` varchar(100) DEFAULT NULL,
+  `description` text,
+  `amount` double DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `branch` */
+
+DROP TABLE IF EXISTS `branch`;
+
+CREATE TABLE `branch` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
 /*Table structure for table `commissioner` */
 
 DROP TABLE IF EXISTS `commissioner`;
 
 CREATE TABLE `commissioner` (
   `id` int(45) NOT NULL AUTO_INCREMENT,
-  `fullname` varchar(200) DEFAULT NULL,
+  `lastname` varchar(200) DEFAULT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `branch` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `datearray` date NOT NULL,
+  `timearray` time NOT NULL,
   `status` varchar(100) DEFAULT 'Active',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `commissionerdetails` */
+
+DROP TABLE IF EXISTS `commissionerdetails`;
+
+CREATE TABLE `commissionerdetails` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `comm_id` varchar(100) DEFAULT NULL,
+  `trainee_id` varchar(100) DEFAULT NULL,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
+  `status` varchar(100) DEFAULT 'pending',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `customer` */
 
@@ -33,11 +77,9 @@ DROP TABLE IF EXISTS `customer`;
 
 CREATE TABLE `customer` (
   `id` int(45) NOT NULL AUTO_INCREMENT,
-  `idnumber` varchar(100) DEFAULT NULL,
+  `controlno` varchar(100) DEFAULT NULL,
   `lastname` varchar(100) DEFAULT NULL,
   `firstname` varchar(100) DEFAULT NULL,
-  `middlename` varchar(100) DEFAULT NULL,
-  `suffix` varchar(20) DEFAULT NULL,
   `type` varchar(100) DEFAULT NULL,
   `code` varchar(100) DEFAULT NULL,
   `amount` double DEFAULT NULL,
@@ -46,8 +88,10 @@ CREATE TABLE `customer` (
   `datearray` date DEFAULT NULL,
   `timearray` time DEFAULT NULL,
   `login_user` varchar(100) DEFAULT NULL,
+  `branch` varchar(100) NOT NULL,
+  `remarks` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `designation` */
 
@@ -57,7 +101,7 @@ CREATE TABLE `designation` (
   `id` int(45) NOT NULL AUTO_INCREMENT,
   `designation` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `employee` */
 
@@ -73,7 +117,7 @@ CREATE TABLE `employee` (
   `birthdate` date DEFAULT NULL,
   `gender` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `employeedetails` */
 
@@ -85,6 +129,19 @@ CREATE TABLE `employeedetails` (
   `designation` varchar(100) DEFAULT NULL,
   `salary` double DEFAULT NULL,
   `is_daily` int(11) DEFAULT '1',
+  `branch` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `expenses` */
+
+DROP TABLE IF EXISTS `expenses`;
+
+CREATE TABLE `expenses` (
+  `id` int(45) NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `datearray` date DEFAULT NULL,
+  `timearray` time DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -99,7 +156,7 @@ CREATE TABLE `user_logs` (
   `datearray` date DEFAULT NULL,
   `timearray` time DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 /*Table structure for table `users` */
 
@@ -111,8 +168,9 @@ CREATE TABLE `users` (
   `password` varchar(100) DEFAULT NULL,
   `fullname` varchar(100) DEFAULT NULL,
   `is_admin` int(1) DEFAULT NULL,
+  `branch` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
