2e2b6cb93215c0fb08d966b441851786f390f5de
shakiel17 <114818927+shakiel17@users.noreply.github.com>
2025-04-24 11:43:11 +0800

    Update print_enrollee.php

--- application/views/pages/print_enrollee.php
+++ application/views/pages/print_enrollee.php
@@ -17,9 +17,9 @@ if($type=="All"){
             <td align="center">NO.</td>
             <td align="center" width="30%">NAME</td>
             <td align="center">TYPE</td>
-            <td align="center">CODE</td>
-            <td align="center">COMM</td>
+            <td align="center">CODE</td>            
             <td align="center">AMOUNT</td>
+            <td align="center">COMM</td>
             <td align="center">STATUS</td>
             <td align="center" width="30%">REMARKS</td>
         </tr>
@@ -32,9 +32,9 @@ if($type=="All"){
                     echo "<td>$x.</td>";
                     echo "<td>$item[lastname], $item[firstname]</td>";
                     echo "<td align='center'>$item[type]</td>";
-                    echo "<td align='center'>$item[code]</td>";
-                    echo "<td align='center'>$comm[firstname]</td>";
+                    echo "<td align='center'>$item[code]</td>";                    
                     echo "<td align='right'>".number_format($item['amount'],2)."</td>";
+                    echo "<td align='center'>$comm[firstname]</td>";
                     echo "<td align='center'>$item[status]</td>";
                     echo "<td align='center' style='font-size:12px;'>$item[remarks]</td>";
                 echo "</tr>";
