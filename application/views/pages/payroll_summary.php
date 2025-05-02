0ace31998cb7b9b286ddab0142858d95804f0d45
shakiel17 <hisoka.aboy@gmail.com>
2025-05-02 17:53:59 +0800

    Update payroll_summary.php

--- application/views/pages/payroll_summary.php
+++ application/views/pages/payroll_summary.php
@@ -8,7 +8,7 @@
     DAILY RATE SALARY
     <table border="1" width="100%" cellspacing="0" cellpadding="1" style="border-collapse: collapse; font-size:14px;">
         <tr>
-            <td align="center">No.</td>
+            <td align="center" width="3%">No.</td>
             <td align="center" width="25%">Name</td>
             <td align="center" width="8%">Rate</td>
             <td align="center" width="10%">Required Days</td>
@@ -17,11 +17,11 @@
             <td align="center" width="10%">Gross Pay</td>
             <td align="center" width="8%">Deduction</td>
             <td align="center" width="9%">Net Pay</td>
-            <td align="center" width="12%">Signature</td>
+            <!-- <td align="center" width="12%">Signature</td> -->
         </tr>
         <?php
         $x=1;
-        $totalnet=0;
+        $totalnetdaily=0;
         $totalgross=0;
         $totaladjustment=0;
         $totaldeduction=0;
@@ -31,8 +31,16 @@
             foreach($adjustment as $row){
                 $adjusttotal += $row['amount'];
             }
-            $net=(($branch['salary']*$branch['no_of_days_work'])+$adjusttotal) - $branch['deduction'];
+            $fixed_deduction=$this->Payroll_model->getAllFixedDeduction($branch['empid']);
+            $fixdeduct=0;
+            foreach($fixed_deduction as $row){
+                $fixdeduct += $row['amount'];
+            }
+            $net=(($branch['salary']*$branch['no_of_days_work'])+$adjusttotal) - $fixdeduct;
             $gross=($branch['salary']*$branch['no_of_days_work'])+$adjusttotal;
+            if($net < 0){
+                $net=0;
+            }
             echo "<tr>";
                 echo "<td>$x.</td>";
                 echo "<td>$branch[lastname], $branch[firstname] $branch[middlename] $branch[suffix]</td>";                                
@@ -41,11 +49,11 @@
                 echo "<td align='center'>$branch[no_of_days_work]</td>";
                 echo "<td align='right'>".number_format($adjusttotal,2)."</td>";
                 echo "<td align='right'>".number_format($gross,2)."</td>";
-                echo "<td align='right'>".number_format($branch['deduction'],2)."</td>";
+                echo "<td align='right'>".number_format($fixdeduct,2)."</td>";
                 echo "<td align='right'><b>".number_format($net,2)."</b></td>";
-                echo "<td>&nbsp;</td>";
+                // echo "<td>&nbsp;</td>";
             echo "</tr>";
-            $totalnet += $net;
+            $totalnetdaily += $net;
             $totalgross += $gross;
             $totaladjustment += $adjusttotal;
             $totaldeduction += $branch['deduction'];
@@ -53,19 +61,19 @@
         }
         ?>
         <tr>
-            <td colspan="5"><b>Total</b></td>
-            <td align="right"><b><?=number_format($totaladjustment,2);?></b></td>
+            <td colspan="8"><b>Total</b></td>
+            <!-- <td align="right"><b><?=number_format($totaladjustment,2);?></b></td>
             <td align="right"><b><?=number_format($totalgross,2);?></b></td>
-            <td align="right"><b><?=number_format($totaldeduction,2);?></b></td>
-            <td align="right"><b><?=number_format($totalnet,2);?></b></td>
-            <td>&nbsp;</td>
+            <td align="right"><b><?=number_format($totaldeduction,2);?></b></td> -->
+            <td align="right"><b><?=number_format($totalnetdaily,2);?></b></td>
+            <!-- <td>&nbsp;</td> -->
         </tr>
     </table>
     <br>
     INSTRUCTOR SALARY
     <table border="1" width="100%" cellspacing="0" cellpadding="1" style="border-collapse: collapse; font-size:14px;">
         <tr>
-            <td align="center">No.</td>
+            <td align="center" width="3%">No.</td>
             <td align="center" width="25%">Name</td>
             <td align="center" width="8%">PDC</td>
             <td align="center" width="8%">TDC</td>            
@@ -73,11 +81,11 @@
             <td align="center" width="13%">Gross Pay</td>
             <td align="center" width="10%">Deduction</td>
             <td align="center" width="13%">Net Pay</td>
-            <td align="center" width="13%">Signature</td>
+            <!-- <td align="center" width="13%">Signature</td> -->
         </tr>
         <?php
         $x=1;
-        $totalnet=0;
+        $totalnetperhead=0;
         $totalgross=0;
         $totaladjustment=0;
         $totaldeduction=0;
@@ -88,10 +96,16 @@
             foreach($adjustment as $row){
                 $adjusttotal += $row['amount'];
             }
+            $fixed_deduction=$this->Payroll_model->getAllFixedDeduction($branch['empid']);
+            $fixdeduct=0;
+            foreach($fixed_deduction as $row){
+                $fixdeduct += $row['amount'];
+            }
             $pdc=$branch['no_of_heads_pdc']*60;
             $tdc=$branch['no_of_heads_tdc']*80;
             $gross=(($pdc+$tdc)/$per_head) + $adjusttotal;
-            $net=$gross - $branch['deduction'];
+            $net=$gross - $fixdeduct;
+            
             echo "<tr>";
                 echo "<td>$x.</td>";
                 echo "<td>$branch[lastname], $branch[firstname] $branch[middlename] $branch[suffix]</td>";                                                
@@ -99,11 +113,11 @@
                 echo "<td align='center'>$branch[no_of_heads_tdc]</td>";
                 echo "<td align='right'>".number_format($adjusttotal,2)."</td>";
                 echo "<td align='right'>".number_format($gross,2)."</td>";
-                echo "<td align='right'>".number_format($branch['deduction'],2)."</td>";
+                echo "<td align='right'>".number_format($fixdeduct,2)."</td>";
                 echo "<td align='right'><b>".number_format($net,2)."</b></td>";
-                echo "<td>&nbsp;</td>";
+                // echo "<td>&nbsp;</td>";
             echo "</tr>";
-            $totalnet += $net;
+            $totalnetperhead += $net;
             $totalgross += $gross;
             $totaladjustment += $adjusttotal;
             $totaldeduction += $branch['deduction'];
@@ -111,12 +125,15 @@
         }
         ?>
         <tr>
-            <td colspan="4"><b>Total</b></td>
-            <td align="right"><b><?=number_format($totaladjustment,2);?></b></td>
+            <td colspan="7"><b>Total</b></td>
+            <!-- <td align="right"><b><?=number_format($totaladjustment,2);?></b></td>
             <td align="right"><b><?=number_format($totalgross,2);?></b></td>
-            <td align="right"><b><?=number_format($totaldeduction,2);?></b></td>
-            <td align="right"><b><?=number_format($totalnet,2);?></b></td>
-            <td>&nbsp;</td>
+            <td align="right"><b><?=number_format($totaldeduction,2);?></b></td> -->
+            <td align="right"><b><?=number_format($totalnetperhead,2);?></b></td>
+            <!-- <td>&nbsp;</td> -->
         </tr>
     </table>
+    <p align="right">
+        <b>TOTAL: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u><?=number_format($totalnetdaily+$totalnetperhead,2);?></u></b>
+    </p>
 </div>
